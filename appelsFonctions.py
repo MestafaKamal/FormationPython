7b83bbf78d72992bf9712ac2fedf272d5122f378
MestafaKamal <em_ben_ramdane@esi.dz>
2019-12-30 15:24:46 +0100

    oui

---
+++ appelsFonctions.py
@@ -0,0 +1,26 @@
+from fonctions import *
+import math
+
+# Les appels
+n = 24
+print("Parité de ", n, " est ", estPaire1(n))
+listerPaires(10)
+
+print("la racinde de 4 est ", math.sqrt(4))
+
+notes1 = [12.5, 8.5, "15", 13.75, 10, 16.25, 18]
+print("longueur", longueur1(notes1))
+
+a = afficherBonjour("Ahmed", 2)
+
+print(a)
+
+# exercice
+# Fonction qui calcule la factorielle d'un nombre
+# def factorielle(a) ---> me renvoie la factorielle
+
+# Fonction qui vérifie si un nombre est mutiple de 2 et 3
+# en même temps
+
+# Fonction qui affiche tous les nombres paires inférieurs à n donné
+# affNbrPaire(n): elle me donne tous les nombres paires < n
--- fonctions.py
+++ fonctions.py
@@ -40,26 +40,3 @@ def listerPaires(n):
             if estPaire(i):
                 print(i)
 
-
-# Les appels
-n = 24
-print("Parité de ", n, " est ", estPaire1(n))
-listerPaires(10)
-
-
-notes1 = [12.5, 8.5, "15", 13.75, 10, 16.25, 18]
-print("longueur", longueur1(notes1))
-
-a = afficherBonjour("Ahmed", 2)
-
-print(a)
-
-# exercice
-# Fonction qui calcule la factorielle d'un nombre
-# def factorielle(a) ---> me renvoie la factorielle
-
-# Fonction qui vérifie si un nombre est mutiple de 2 et 3
-# en même temps
-
-# Fonction qui affiche tous les nombres paires inférieurs à n donné
-# affNbrPaire(n): elle me donne tous les nombres paires < n
